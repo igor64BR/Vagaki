dfe8e5998323c4d9db04ea7dd5ddabff047aa17b
Igor Baiocco <igor64br@gmail.com>
2021-06-16 22:22:03 -0300

    Package installation and SQL changed

--- vagaki/settings.py
+++ vagaki/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Extra django apps
     'bootstrap4',
+    # 'django-stdimage'  # Media files
     # Local apps
     'core',
 ]
@@ -78,12 +81,18 @@ WSGI_APPLICATION = 'vagaki.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+Substituted by PostgreSQL bellow
+"""
+# Using PostgreSQL with Heroku
+DATABASES = {
+    'default': dj_database_url.config()
 }
 
 
@@ -124,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
